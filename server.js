918432ba81697fa5cb9596aa20b4a99fc459b39e
Dave Darch <macstudio@DavesMacStudio.lan>
2024-11-08 10:37:40 +0000

    initial commit of database branch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Serve the main HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Example route to get all words
+app.get('/api/words', (req, res) => {
+  const words = db.prepare('SELECT * FROM words').all();
+  res.json(words);
+});
+
+// Example route to add a new word
+app.post('/api/words', (req, res) => {
+  const { title, thing, synonym_01, synonym_02, whatis_01, whatis_02, whyuse_01, whyuse_02, whatdo_01, whatdo_02, simile_01, simile_02, thing_img_01, thing_img_02, simile_img_01, simile_img_02 } = req.body;
+  const stmt = db.prepare('INSERT INTO words (title, thing, synonym_01, synonym_02, whatis_01, whatis_02, whyuse_01, whyuse_02, whatdo_01, whatdo_02, simile_01, simile_02, thing_img_01, thing_img_02, simile_img_01, simile_img_02) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+  stmt.run(title, thing, synonym_01, synonym_02, whatis_01, whatis_02, whyuse_01, whyuse_02, whatdo_01, whatdo_02, simile_01, simile_02, thing_img_01, thing_img_02, simile_img_01, simile_img_02);
+  res.sendStatus(201);
 });
 
 app.listen(PORT, () => {
